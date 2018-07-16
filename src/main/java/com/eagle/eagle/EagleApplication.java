332093dba53205636a7f89b690831523b2cc7dd4
zhuyundong <yundong.zhu@sy-fintech.com>
2018-07-16 18:20:56 +0800

    initialization project

---
+++ src/main/java/com/eagle/eagle/EagleApplication.java
@@ -0,0 +1,12 @@
+package com.eagle.eagle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleApplication.class, args);
+	}
+}
